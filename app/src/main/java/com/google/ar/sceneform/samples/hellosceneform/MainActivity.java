24c1791b10df2c873e425fea2d6c39d7c5370525
Ivan Potapov <potapovivan999@gmail.com>
2019-03-27 16:51:14 +0200

    draw line between two nodes

--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MainActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MainActivity.java
@@ -8,27 +8,50 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = "test_" + MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private static final String FIRST_NODE_NAME = "first_node";
+    private static final String SECOND_NODE_NAME = "second_node";
 
     private ArFragment arFragment;
 
-    private ModelRenderable firstNode;
-    private ModelRenderable secondNode;
-    private ModelRenderable thirdNode;
+    private AnchorNode firstAnchorNode;
+    private AnchorNode secondAnchorNode;
+
+    private TransformableNode firstNode;
+    private TransformableNode secondNode;
+
+    private Node lineNode;
+    private Node labelNode;
+
+    private ViewRenderable tvLabelRenderable;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +63,65 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_ux);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        if (arFragment != null) {
+            arFragment.setOnTapArPlaneListener(
+                    (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                        if (firstAnchorNode == null) {
+                            firstAnchorNode = createAndDrawNode(hitResult, FIRST_NODE_NAME);
+                        } else if (secondAnchorNode == null) {
+                            secondAnchorNode = createAndDrawNode(hitResult, SECOND_NODE_NAME);
+                            lineNode = createAndDrawLineBetweenTwoPositions(
+                                    firstAnchorNode.getWorldPosition(),
+                                    secondAnchorNode.getWorldPosition()
+                            );
 
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    // Create the Anchor.
-                    Anchor anchor = hitResult.createAnchor();
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-                });
+                            secondAnchorNode.addTransformChangedListener((node, node1) -> updateLine(
+                                    lineNode,
+                                    firstNode.getWorldPosition(),
+                                    secondNode.getWorldPosition()));
+                        }
+                    });
+        }
+
+        ViewRenderable.builder()
+                .setView(this, R.layout.label_layout)
+                .build().thenAccept(viewRenderable -> tvLabelRenderable = viewRenderable);
+
+    }
+
+    public AnchorNode createAndDrawNode(HitResult hitResult, String name) {
+
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+        drawSphere(anchorNode, name);
+        return anchorNode;
+    }
+
+    private void drawSphere(AnchorNode anchorNode, String name) {
+        MaterialFactory.makeTransparentWithColor(getApplicationContext(), new Color(0, 0, 0))
+                .thenAccept(
+                        material -> {
+
+                            ModelRenderable model = ShapeFactory.makeSphere(0.015f, Vector3.zero(), material);
+                            model.setShadowCaster(false);
+                            model.setShadowReceiver(false);
+
+                            TransformableNode node = new TransformableNode(
+                                    arFragment.getTransformationSystem()
+                            );
+
+                            node.setParent(anchorNode);
+                            node.setRenderable(model);
+                            node.setName(name);
+
+                            if (FIRST_NODE_NAME.equals(name)) {
+                                firstNode = node;
+                            } else if (SECOND_NODE_NAME.equals(name)) {
+                                secondNode = node;
+                            }
+
+                        });
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
@@ -64,5 +138,56 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private Node createAndDrawLineBetweenTwoPositions(Vector3 pos1, Vector3 pos2) {
+
+        final Vector3 difference = Vector3.subtract(pos1, pos2);
+        final Vector3 directionFromTopToBottom = difference.normalized();
+        final Quaternion rotationFromAToB =
+                Quaternion.lookRotation(directionFromTopToBottom, Vector3.up());
+
+        Node node = new Node();
+        MaterialFactory.makeOpaqueWithColor(getApplicationContext(), new Color(0, 0, 0))
+                .thenAccept(
+                        material -> {
+
+                            ModelRenderable model = ShapeFactory.makeCube(
+                                    new Vector3(.01f, .01f, difference.length()),
+                                    Vector3.zero(), material);
+
+                            model.setShadowCaster(false);
+                            model.setShadowReceiver(false);
+
+                            node.setParent(firstAnchorNode);
+                            node.setRenderable(model);
+                            node.setWorldPosition(Vector3.add(pos1, pos2).scaled(.5f));
+                            node.setWorldRotation(rotationFromAToB);
+                        }
+                );
+        return node;
+    }
+
+    private void updateLine(Node node, Vector3 pos1, Vector3 pos2) {
+        final Vector3 difference = Vector3.subtract(pos1, pos2);
+        final Vector3 directionFromTopToBottom = difference.normalized();
+        final Quaternion rotationFromAToB =
+                Quaternion.lookRotation(directionFromTopToBottom, Vector3.up());
+
+        MaterialFactory.makeOpaqueWithColor(getApplicationContext(), new Color(0, 0, 0))
+                .thenAccept(
+                        material -> {
+
+                            ModelRenderable model = ShapeFactory.makeCube(
+                                    new Vector3(.01f, .01f, difference.length()),
+                                    Vector3.zero(), material);
+
+                            model.setShadowCaster(false);
+                            model.setShadowReceiver(false);
+                            node.setRenderable(model);
+                            node.setWorldPosition(Vector3.add(pos1, pos2).scaled(.5f));
+                            node.setWorldRotation(rotationFromAToB);
+                        }
+                );
     }
 }
